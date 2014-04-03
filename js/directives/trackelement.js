3418c9f85c3215ad8487a0fd002b80b82447b837
Jesse Hu <huyouare@gmail.com>
2014-04-03 19:35:22 -0400

    Changes to trackManager

--- js/directives/trackelement.js
+++ js/directives/trackelement.js
@@ -2,9 +2,8 @@ var TrackElement = (function() {
 	function TrackElement(element, track){
 		this.el = element;
 		this.track = track;
-		this.el.id = this.track.name
-		this.el.innerHTML = 
-		document.querySelector('#track-template').innerHTML;
+		this.el.id = this.track.name;
+		this.el.innerHTML = document.querySelector('#track-template').innerHTML;
 
 		this.track = track;
 		this.el.querySelector('.name').innerText = this.track.name;
@@ -19,23 +18,29 @@ var TrackElement = (function() {
 
 		this.el.querySelector('.del')
 		.addEventListener('click', this.del.bind(this));
+
+		this.el.querySelector('.load')
+		.addEventListener('click', this.load.bind(this));
 	}
 
 	TrackElement.prototype.name = function() {//populate the track editor
 		window.dispatchEvent(new CustomEvent('track.editor', {detail: this.track}));
-	}
+	};
 
 	TrackElement.prototype.play = function() {
 		window.dispatchEvent(new CustomEvent('track.play', {detail: this.track}));
-	}
+	};
 
 	TrackElement.prototype.stop = function() {
 		window.dispatchEvent(new CustomEvent('track.stop', {detail: this.track}));
-	}
+	};
 
 	TrackElement.prototype.del = function() {
 		window.dispatchEvent(new CustomEvent('track.del', {detail: this.track}));
-	}
+	};
 
+	TrackElement.prototype.load = function() {
+		window.dispatchEvent(new CustomEvent('track.load', {detail: this.track}));
+	};
 	return TrackElement;
 })();
